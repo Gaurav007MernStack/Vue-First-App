0943190db4cf13a4648a7e5cef1540ff9417d10c
Gaurav Singh <singhgaurav1998gs@gmail.com>
2021-05-12 12:56:26 +0530

    +gaurav Creatd an Vue Timer Game

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time - {{ score }} ms </p>
+  <p class="rank">{{ rank }} </p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 350){
+            this.rank = "Super Fast"
+        } else if (this.score < 500 ){
+            this.rank = 'Intermediate, can do Better'
+        } else {
+            this.rank = 'Slow ...'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
